fad0f17f3f7b90bbfccc999b6ed8ec28b72196d6
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2024-05-14 04:48:17 -0700

    added kodachan font and applied to About

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,19 @@ export default function Home() {
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			{/* About */}
 			<div className="flex justify-center">
-				<div className="container text-center">
-					<h1 className={title({ color: "violet" })}>Brittney Cavazos</h1>
+				<div className="container text-center kodchasan-bold">
+					<h1 className={title({ color: "violet" })}>
+						Brittney Cavazos
+					</h1>
 					<br />
 					<p className={subtitle({ class: "mt-4" })}>
 						Front end, mobile development, logo, & print design
 					</p>
 				</div>
 				<ColorContainer
-					classProp={"max-w-lg justify-center montserrat leading-relaxed"}
+					classProp={
+						"max-w-lg justify-center montserrat leading-relaxed"
+					}
 					fillings={HomeCopy.description}
 				/>
 			</div>
