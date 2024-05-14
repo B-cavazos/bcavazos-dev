6b88b17d0e14b3237d3e2ece297e523c990b4941
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2024-05-14 05:18:43 -0700

    added extended colors to tailwind congig, added vscode extensions to gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,38 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        "Kodchasan": ["Kodchasan","sans-serif",]
-      }
-    },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+	content: [
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				Kodchasan: ["Kodchasan", "sans-serif"],
+			},
+			colors: {
+				sand: "#FBF6EF",
+				plum: "#712E85",
+				lilac: "#B59FBD",
+				lilac2: "#E7C2FF",
+				indigo: "#291F84",
+				sky: "5B94FF",
+				orange: "#ECA193",
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [nextui()],
+};
+
+//purple-950 -- purple text
+//violet-300 -- inner shadow top
+//violet-400 -- header text
+//violet-600
+//indigo-800 -- shadow
+//indigo--950 -- blue text
+//amber-50 -- bg
+//amber-600 -- inner shadow bottom
+//blue-500 -- divider
