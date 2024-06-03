db77f3b7daa22fbb7e8f66591755042483990bbf
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2024-06-02 17:08:42 -0700

    layout.tsx- remove crossorigin from ln 37 - reference https://stackoverflow.com/questions/69377275/typescript-font-import-error-with-link-tag-property-crossorigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,11 +34,7 @@ export default function RootLayout({
 		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
 					rel="stylesheet"
