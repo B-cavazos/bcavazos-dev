f42d59b19791fc670a0adae16989eea610a5c688
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2024-06-03 17:34:53 -0700

    nextConfig - add basePath and assetPrefix , create ,env and add base path variable, site.ts - update iamge paths to dynamically include the base path from .env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  basePath: '/bcavazos-dev',
+  assetPrefix: '/bcavazos-dev/',
 }
 
 module.exports = nextConfig
